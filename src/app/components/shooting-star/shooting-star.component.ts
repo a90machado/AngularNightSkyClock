52f6b6a7769cac2705c53aaa2bac5954f19b5ce0
a90machado <a90machado@gmail.com>
2025-04-03 13:55:10 +0100

    WIP

---
+++ src/app/components/shooting-star/shooting-star.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shooting-star',
+  standalone: true,
+  imports: [],
+  templateUrl: './shooting-star.component.html',
+  styleUrl: './shooting-star.component.scss'
+})
+export class ShootingStarComponent {
+
+}
